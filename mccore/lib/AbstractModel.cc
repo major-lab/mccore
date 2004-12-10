3d806a9706ec6d5c76eca1d2a9e08ba94889a411
larosem <larosem>
2004-12-10 00:29:33 +0000

    New Files.

---
+++ mccore/lib/AbstractModel.cc
@@ -0,0 +1,312 @@
+//                              -*- Mode: C++ -*- 
+// AbstractModel.cc
+// Copyright © 2004 Laboratoire de Biologie Informatique et Théorique
+//                  Université de Montréal.
+// Author           : Martin Larose <larosem@iro.umontreal.ca>
+// Created On       : Thu Dec  9 16:46:03 2004
+// $Revision: 1.1 $
+// $Id: AbstractModel.cc,v 1.1 2004-12-10 00:29:33 larosem Exp $
+// 
+// This file is part of mccore.
+// 
+// mccore is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// mccore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with mccore; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <algorithm>
+#include <string.h>
+
+#include "AbstractModel.h"
+#include "Binstream.h"
+#include "ExtendedResidue.h"
+#include "Pdbstream.h"
+#include "Residue.h"
+#include "ResidueFactoryMethod.h"
+
+
+
+namespace mccore
+{
+
+  AbstractModel::AbstractModel (const ResidueFactoryMethod *fm)
+    : residueFM (0 == fm ? new ExtendedResidueFM () : fm->clone ())
+  { }
+
+  
+  AbstractModel::AbstractModel (const AbstractModel &right)
+    : residueFM (right.residueFM->clone ())
+  { }
+
+
+  AbstractModel::~AbstractModel ()
+  {
+    delete this->residueFM;
+  }
+
+
+  AbstractModel&
+  AbstractModel::operator= (const AbstractModel &right)
+  {
+    delete residueFM;
+    residueFM = right.residueFM->clone ();
+    return *this;
+  }
+
+  
+  void
+  AbstractModel::setResidueFM (const ResidueFactoryMethod *fm)
+  {
+    delete this->residueFM;
+    this->residueFM = 0 == fm ? new ExtendedResidueFM () : fm->clone ();
+  }
+
+
+  AbstractModel::iterator
+  AbstractModel::find (const ResId &id)
+  {
+    iterator it;
+
+    for (it = begin (); it != end (); ++it)
+      if (id == it->getResId ())
+	break;
+    return it;
+  }
+
+
+  AbstractModel::const_iterator
+  AbstractModel::find (const ResId &id) const
+  {
+    const_iterator it;
+
+    for (it = begin (); it != end (); ++it)
+      if (id == it->getResId ())
+	break;
+    return it;
+  }
+  
+  
+  void
+  AbstractModel::validate ()
+  {
+    iterator it = begin ();
+    
+    while (it != end ())
+      {
+	it->validate ();
+	if (it->getType ()->isNucleicAcid ()
+	    || it->getType ()->isAminoAcid ())
+	  ++it;
+	else
+	  {
+	    delete &*it;
+	    it = erase (it);
+	  }
+      }
+  }
+
+  
+  void
+  AbstractModel::addHLP (bool overwrite)
+  {
+    iterator it;
+    
+    for (it = begin (); it != end (); ++it)
+      {
+	it->addHydrogens (overwrite);
+	it->addLonePairs (overwrite);
+      }
+  }
+
+  
+  void
+  AbstractModel::removeOptionals ()
+  {
+    iterator it;
+
+    for (it = begin (); it != end (); ++it)
+      it->removeOptionals ();
+  }
+
+
+  void
+  AbstractModel::removeAminoAcid ()
+  {
+    iterator modelIt = begin ();
+
+    while (modelIt != end ())
+      {
+	if (modelIt->getType ()->isAminoAcid ())
+	  {
+	    delete &*modelIt;
+	    modelIt = erase (modelIt);
+	  }
+	else
+	  ++modelIt;
+      }
+  }
+
+
+  void
+  AbstractModel::removeNucleicAcid ()
+  {
+    iterator modelIt;
+
+    modelIt = begin ();
+
+    while (modelIt != end ())
+      {
+	if (modelIt->getType ()->isNucleicAcid ())
+	  {
+	    delete &*modelIt;
+	    modelIt = erase (modelIt);
+	  }
+	else
+	  ++modelIt;
+      }
+  }
+
+
+  void
+  AbstractModel::removeWater ()
+  {
+    const ResidueType* water = ResidueType::parseType ("HOH");
+    iterator modelIt;
+
+    modelIt = begin ();
+    while (modelIt != end ())
+      {
+	if (modelIt->getType () == water)
+	  {
+	    delete &*modelIt;
+	    modelIt = erase (modelIt);
+	  }
+	else
+	  ++modelIt;
+      }
+  }
+
+
+  void
+  AbstractModel::keepAminoAcid ()
+  {
+    iterator modelIt;
+
+    for (modelIt = begin (); modelIt != end ();)
+      {
+	if (modelIt->getType ()->isAminoAcid ())
+	  ++modelIt;
+	else
+	  {
+	    delete &*modelIt;
+	    modelIt = erase (modelIt);
+	  }
+      }
+  }
+
+
+  void
+  AbstractModel::keepNucleicAcid ()
+  {
+    iterator modelIt;
+
+    for (modelIt = begin (); modelIt != end ();)
+      {
+	if (modelIt->getType ()->isNucleicAcid ())
+	  ++modelIt;
+	else
+	  {
+	    delete &*modelIt;
+	    modelIt = erase (modelIt);
+	  }
+      }
+  }
+
+
+  oPdbstream&
+  AbstractModel::output (oPdbstream &ops) const
+  {
+    const_iterator cit;
+    char chainId;
+
+    cit = begin ();
+    if (end () != cit)
+      {
+	chainId = cit->getResId ().getChainId ();
+	for (; end () != cit; ++cit)
+	  {
+	    if (cit->getResId ().getChainId () != chainId)
+	      {
+		ops.ter ();
+		chainId = cit->getResId ().getChainId ();
+	      }
+	    ops << *cit;
+	  }
+	ops.ter ();
+      }
+    return ops;
+  }
+  
+  
+  ostream&
+  operator<< (ostream &os, const AbstractModel &obj)
+  {
+    return obj.output (os);
+  }
+
+
+  iPdbstream&
+  operator>> (iPdbstream &ips, AbstractModel &obj)
+  {
+    return obj.input (ips);
+  }
+
+
+  oPdbstream&
+  operator<< (oPdbstream &ops, const AbstractModel &obj)
+  {
+    return obj.output (ops);
+  }
+
+
+  iBinstream&
+  operator>> (iBinstream &ibs, AbstractModel &obj)
+  {
+    return obj.input (ibs);
+  }
+
+
+  oBinstream&
+  operator<< (oBinstream &obs, const AbstractModel &obj)
+  {
+    return obj.output (obs);
+  }
+
+
+  bool
+  operator< (const AbstractModel::iterator &left, const AbstractModel::iterator &right)
+  {
+    return *left < *right;
+  }
+
+
+  bool
+  operator< (const AbstractModel::const_iterator &left, const AbstractModel::const_iterator &right)
+  {
+    return *left < *right;
+  }
+}
---
+++ mccore/lib/AbstractModel.h
@@ -0,0 +1,488 @@
+//                              -*- Mode: C++ -*- 
+// AbstractModel.h
+// Copyright © 2004 Laboratoire de Biologie Informatique et Théorique
+//                  Université de Montréal.
+// Author           : Martin Larose <larosem@iro.umontreal.ca>
+// Created On       : Thu Dec  9 16:12:42 2004
+// $Revision: 1.1 $
+//
+// This file is part of mccore.
+// 
+// mccore is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// mccore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with mccore; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#ifndef _AbstractModel_h_
+#define _AbstractModel_h_
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+namespace mccore
+{
+  class ResId;
+  class Residue; 
+  class ResidueFactoryMethod;
+  class iBinstream;
+  class iPdbstream;
+  class oBinstream;
+  class oPdbstream;
+
+
+
+  /**
+   * @short Container for residues using a simple list.
+   *
+   * This object is the container for residues.  It is an unsorted list
+   * of residues.  Random access is simulated with sequential access.
+   * This class should be the replacement for nearly all Residue containers.
+   *
+   * NOTES:
+   *
+   * - The container contains pointers to residues but dereferenced
+   *   iterators yields object.
+   *
+   *  - Please do not change the STL container (see removeClash for
+   * details).
+   *
+   * @author Martin Larose <larosem@iro.umontreal.ca>
+   * @version $Id: AbstractModel.h,v 1.1 2004-12-10 00:29:33 larosem Exp $
+   */
+  class AbstractModel
+  {
+    
+  public:
+    
+    typedef unsigned int size_type;
+
+  protected:
+
+    /**
+     * Factory method for creating new residues.
+     */
+    ResidueFactoryMethod *residueFM;
+
+  public:
+
+    // ITERATORS --------------------------------------------------------------
+
+    /**
+     * @short Iterator class for AbstractModel.
+     *
+     * @author Martin Larose <larosem@iro.umontreal.ca>
+     */
+    class model_iterator : public vector< Residue* >::iterator
+    {
+    public:
+
+      // LIFECYCLE ------------------------------------------------------------
+
+      /**
+       * Initializes the iterator.
+       */
+      model_iterator () : vector< Residue* >::iterator () { }
+    
+      /**
+       * Initializes the iterator with the list iterator.
+       * @param lIt the list iterator.
+       */
+      model_iterator (const vector< Residue* >::iterator &lIt)
+	: vector< Residue* >::iterator (lIt)
+      { }
+
+      // OPERATORS ------------------------------------------------------------
+
+      /**
+       * Redefines the access operator* to get the dereferenced residue.
+       * @return the referenced residue.
+       */
+      Residue& operator* () const
+      { 
+	return *vector< Residue* >::iterator::operator* (); 
+      }
+
+      /**
+       * Redefines the access operator-> to get the dereferenced residue.
+       * @return the pointed residue.
+       */
+      Residue* operator-> () const { return &(operator* ()); }
+    };
+  
+    /**
+     * @short Const iterator class for AbstractModel.
+     *
+     * @author Martin Larose <larosem@iro.umontreal.ca>
+     */
+    class model_const_iterator : public vector< Residue* >::const_iterator
+    {
+    public:
+
+      /**
+       * Initializes the const iterator.
+       */
+      model_const_iterator () : vector< Residue* >::const_iterator () { }
+    
+      /**
+       * Initializes the iterator with the list iterator.
+       * @param lIt the list iterator.
+       */
+      model_const_iterator (const vector< Residue* >::const_iterator &lIt)
+	: vector< Residue* >::const_iterator (lIt)
+      { }
+
+      /**
+       * Initializes the iterator with a non const model_iterator.
+       * @param it the model iterator.
+       */
+      model_const_iterator (const model_iterator& it)
+	: vector< Residue* >::const_iterator (it)
+      { }
+
+      // OPERATORS ------------------------------------------------------------
+      
+      /**
+       * Redefines the access operator* to get the dereferenced residue.
+       * @return the referenced residue.
+       */
+      const Residue& operator* () const
+      { return *vector< Residue* >::const_iterator::operator* (); }
+
+      /**
+       * Redefines the access operator-> to get the dereferenced residue.
+       * @return the pointed residue.
+       */
+      const Residue* operator-> () const { return &(operator* ()); }
+    };
+
+  public:
+
+    typedef model_iterator iterator;
+    typedef model_const_iterator const_iterator;
+  
+    // LIFECYCLE ------------------------------------------------------------
+
+  protected:
+    
+    /**
+     * Initializes the object.
+     * @param fm the residue factory methods that will instanciate new residues (default is @ref ExtendedResidueFM).
+     */
+    AbstractModel (const ResidueFactoryMethod *fm = 0);
+
+    /**
+     * Initializes the object with the right's content.
+     * @param right the object to copy.
+     */
+    AbstractModel (const AbstractModel &right);
+
+  public:
+    
+    /**
+     * Clones the model.
+     * @return a copy of the model.
+     */
+    virtual AbstractModel* clone () const = 0;
+
+    /**
+     * Destroys the object.
+     */
+    virtual ~AbstractModel ();
+
+    // OPERATORS ------------------------------------------------------------
+
+  protected:
+    
+    /**
+     * Assigns the object with the right's content.
+     * @param right the object to copy.
+     * @return itself.
+     */
+    AbstractModel& operator= (const AbstractModel &right);
+
+  public:
+
+    /**
+     * Gets the model reference at nth position.
+     * @param nth the position of the reference to get.
+     * @return the nth reference.
+     */
+    virtual Residue& operator[] (size_type nth) = 0;
+
+    /**
+     * Gets the model const_reference at nth position.
+     * @param nth the position of the const_reference to get.
+     * @return the nth const_reference.
+     */
+    virtual const Residue& operator[] (size_type nth) const = 0;
+
+    // ACCESS ---------------------------------------------------------------
+
+    /**
+     * Gets the residue factory method.
+     * @return the residue factory method.
+     */
+    const ResidueFactoryMethod* getResidueFM () const { return residueFM; }
+  
+    /**
+     * Sets the residue factory method.
+     * @param fm the new factory method to use.
+     */
+    void setResidueFM (const ResidueFactoryMethod *fm = 0);
+  
+    /**
+     * Gets the iterator pointing to the beginning of the model.
+     * @return the iterator.
+     */
+    virtual iterator begin () = 0;
+
+    /**
+     * Gets the iterator pointing to the end of the model.
+     * @return the iterator.
+     */
+    virtual iterator end () = 0;
+
+    /**
+     * Gets the const iterator pointing to the beginning of the model.
+     * @return the iterator.
+     */
+    virtual const_iterator begin () const = 0;
+
+    /**
+     * Gets the const iterator pointing to the end of the model.
+     * @return the iterator.
+     */
+    virtual const_iterator end () const = 0;
+
+    // METHODS -------------------------------------------------------------
+
+    /**
+     * Inserts a residue at the end.  It calls the vector<> method.
+     * @param res the residue to insert.
+     * @return the position where the residue was inserted.
+     */
+    virtual iterator insert (const Residue &res) = 0;
+
+    /**
+     * Inserts the residue range before pos.  It calls the vector<> method.
+     * @param pos the iterator where the residue will be placed.
+     * @param f the first iterator in the range.
+     * @param l the last iterator in the range.
+     */
+    template <class InputIterator>
+    void insert(InputIterator f, InputIterator l)
+    {
+      while (f != l)
+	{
+	  insert (*f);
+	  ++f;
+	}
+    }
+
+    /**
+     * Erases a residue from the model.
+     * @param pos the position to erase.
+     * @return an iterator on the next residue.
+     */ 
+    virtual iterator erase (iterator pos) = 0;
+    
+    /**
+     * Finds a residue given it's residue id.  Returns an iterator
+     * pointing to the residue or the end of the container if the residue was
+     * not found.
+     * @param id the residue id.
+     * @return a AbstractModel iterator.
+     */
+    iterator find (const ResId &id);
+
+    /**
+     * Finds a residue given it's residue id.  Returns a AbstractModel iterator
+     * pointing to the residue or the end of the container if the residue was
+     * not found.
+     * @param id the residue id.
+     * @return a AbstractModel iterator.
+     */
+    const_iterator find (const ResId &id) const;
+    
+    /**
+     * Sorts the model according to the Residue::operator<
+     */ 
+    virtual void sort () = 0;
+    
+    /**
+     * Returns the number of residues present in the model.
+     * @return a number of residues.
+     */
+    virtual size_type size () const = 0;
+
+    /**
+     * Tells if there is no residue in the model.
+     * @return whether the model is empty.
+     */
+    virtual bool empty () const = 0;
+
+    /**
+     * Removes all of the residues from the model.  
+     */
+    virtual void clear () = 0;
+
+    /**
+     * Validates residues contained in the model.  The validated residues (amino
+     * acid and nucleic acid) are retained, the others deleted.
+     */
+    void validate ();
+
+    /**
+     * Adds hydrogens and lone pairs to each nucleic acid residue. The
+     * terminal HO3' is not added by this method.
+     * @param overwrite Flag used to force the placing of an atom even if
+     * it's already there (default: true)
+     */
+    void addHLP (bool overwrite = true);
+    
+    /**
+     * Removes the optional atoms within the residues.
+     */
+    void removeOptionals ();
+
+    /**
+     * Removes the amino acids from the model.
+     */
+    void removeAminoAcid ();    
+
+    /**
+     * Removes the nucleic acids from the model.
+     */
+    void removeNucleicAcid ();
+
+    /**
+     * Removes the water from the model.
+     */
+    void removeWater ();
+
+    /**
+     * Removes every residue that is not an amino acid.
+     */
+    void keepAminoAcid ();
+
+    /**
+     * Removes every residue that is not a nucleic acid.
+     */
+    void keepNucleicAcid ();
+
+    // I/O  -----------------------------------------------------------------
+
+    /**
+     * Ouputs the model to the stream.
+     * @param os the output stream.
+     * @return the used output stream.
+     */
+    virtual ostream& output (ostream &os) const = 0;
+
+    /**
+     * Writes the model to a pdb output stream.
+     * @param ops the pdb data stream.
+     * @return the consumed pdb stream.
+     */
+    oPdbstream& output (oPdbstream &ops) const;
+
+    /**
+     * Reads the model from a pdb input stream.
+     * @param ips the pdb data stream.
+     * @return the consumed pdb stream.
+     */
+    virtual iPdbstream& input (iPdbstream &ips) = 0;
+  
+    /**
+     * Writes the model to a binary output stream.
+     * @param obs the binary data stream.
+     * @return the consumed binary stream.
+     */
+    virtual oBinstream& output (oBinstream &obs) const = 0;
+
+    /**
+     * Reads the model from a binary input stream.
+     * @param obs the binary data stream.
+     * @return the consumed binary stream.
+     */
+    virtual iBinstream& input (iBinstream &ibs) = 0;
+
+  };
+
+
+  // NON-MEMBER FUNCTION -------------------------------------------------------
+
+  /**
+   * Outputs the model to an output stream.
+   * @param obs the output stream.
+   * @param obj the model to output.
+   * @return the output stream.
+   */
+  ostream& operator<< (ostream &obs, const AbstractModel &obj);
+
+  /**
+   * Inputs the model from a pdb stream.
+   * @param ips the input pdb stream.
+   * @param obj the model where to put the residues.
+   * @return the input pdb stream.
+   */
+  iPdbstream& operator>> (iPdbstream &ips, AbstractModel &obj);
+
+  /**
+   * Outputs the model to a pdb stream.  Termination between residue
+   * chains are made.
+   * @param ops the output pdb stream.
+   * @param obj the model to output.
+   * @return the output pdb stream.
+   */
+  oPdbstream& operator<< (oPdbstream &ops, const AbstractModel &obj);
+
+  /**
+   * Inputs the model from a binary stream.
+   * @param ibs the input binary stream.
+   * @param obj the model where to put the residues.
+   * @return the input binary stream.
+   */
+  iBinstream& operator>> (iBinstream &ibs, AbstractModel &obj);
+
+  /**
+   * Outputs the model to a binary stream.
+   * @param obs the output binary stream.
+   * @param obj the model to output.
+   * @return the output binary stream.
+   */
+  oBinstream& operator<< (oBinstream &obs, const AbstractModel &obj);
+
+  /**
+   * Test the partial order (Residue order) between model iterators.
+   * @param left the left operand.
+   * @param right the right operand.
+   * @return wheter the residue pointed by left is less than right.
+   */
+  bool operator< (const AbstractModel::iterator &left, const AbstractModel::iterator &right);
+
+  /**
+   * Test the partial order (Residue order) between model const_iterators.
+   * @param left the left operand.
+   * @param right the right operand.
+   * @return wheter the residue pointed by left is less than right.
+   */
+  bool operator< (const AbstractModel::const_iterator &left, const AbstractModel::const_iterator &right);
+
+}
+
+#endif
