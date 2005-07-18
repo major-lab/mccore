769c11d589b901da734b5d7e67ca0699595cf683
Philippe Thibault <philippe.thibault2@usherbrooke.ca>
2005-07-18 20:12:33 +0000

    Using AC_CANONICAL_HOST variables instead of calling uname

--- mccore/lib/Version.cc
+++ mccore/lib/Version.cc
@@ -4,8 +4,8 @@
 //                  Université de Montréal.
 // Author           : Philippe Thibault <philippe.thibault@umontreal.ca>
 // Created On       : Wed May 11 10:07:28 2005
-// $Revision: 1.2 $
-// $Id: Version.cc,v 1.2 2005-06-07 22:15:46 larosem Exp $
+// $Revision: 1.3 $
+// $Id: Version.cc,v 1.3 2005-07-18 20:12:30 thibaup Exp $
 // 
 
 #ifdef HAVE_CONFIG_H
@@ -13,29 +13,103 @@
 #endif
 
 
+#include "Binstream.h"
+#include "Messagestream.h"
 #include "Version.h"
 
 
 namespace mccore
 {
-
   const string Version::version (VERSION);
-  const string Version::machine (MACHINE);
-  const string Version::kernel (KERNEL_VERSION);
-  const string Version::osname (OSNAME);
+  const string Version::cpu (VERSION_CPU);
+  const string Version::vendor (VERSION_VENDOR);
+  const string Version::os (VERSION_OS);
+  const string Version::date (__DATE__);
+
+
+  Version::Version ()
+    : current_version (Version::version),
+      current_cpu (Version::cpu),
+      current_vendor (Version::vendor),
+      current_os (Version::os),
+      current_date (Version::date)
+  {
+
+  }
+
 
   ostream&
   Version::write (ostream& os) const
   {
     return os << PACKAGE << " " 
-	      << Version::version << " " 
-	      << Version::machine << " "
-	      << Version::kernel << " "
-	      << Version::osname << " "
-	      << __DATE__;
+	      << this->current_version << " " 
+	      << this->current_cpu << " "
+	      << this->current_vendor << " "
+	      << this->current_os << " "
+	      << this->current_date;
   }
 
 
+  oBinstream&
+  Version::write (oBinstream& obs) const
+  {
+    return obs << this->current_version
+	       << this->current_cpu
+	       << this->current_vendor
+	       << this->current_os
+	       << this->current_date;
+  }
+
+
+  iBinstream& 
+  Version::read (iBinstream& ibs)
+  {
+    this->current_version = this->current_cpu = this->current_vendor = this->current_os = this->current_date = "unread";
+    ibs >> this->current_version 
+	>> this->current_cpu 
+	>> this->current_vendor 
+	>> this->current_os
+	>> this->current_date;
+
+    if (Version::version != this->current_version)
+      gErr (4) << "Warning: reading data created from " << PACKAGE 
+	       << " version " << this->current_version
+	       << " using " << PACKAGE << " version " << Version::version << endl;
+
+    if (Version::cpu != this->current_cpu)
+      gErr (4) << "Warning: reading data created from " << PACKAGE 
+	       << " built on cpu type \"" << this->current_cpu
+	       << "\" using " << PACKAGE << " built on cpu type \"" 
+	       << Version::cpu << "\"" << endl;
+
+    if (Version::vendor != this->current_vendor)
+      gErr (4) << "Warning: reading data created from " << PACKAGE 
+	       << " built on cpu vendor \"" << this->current_vendor
+	       << "\" using " << PACKAGE << " built on cpu vendor \"" 
+	       << Version::vendor << "\"" << endl;
+
+    if (Version::os != this->current_os)
+      gErr (4) << "Warning: reading data created from " << PACKAGE 
+	       << " built on os type \"" << this->current_os
+	       << "\" using " << PACKAGE << " built on os type \"" 
+	       << Version::os << "\"" << endl;
+
+    return ibs;
+  }
+
+
+  oBinstream& 
+  operator<< (oBinstream &obs, const Version& obj)
+  {
+    return obj.write (obs);
+  }
+
+
+  iBinstream& 
+  operator>> (iBinstream &ibs, Version& obj)
+  {
+    return obj.read (ibs);
+  }
 }
 
 namespace std
--- mccore/lib/Version.h
+++ mccore/lib/Version.h
@@ -4,8 +4,8 @@
 //                  Université de Montréal.
 // Author           : Philippe Thibault <philippe.thibault@umontreal.ca>
 // Created On       : Wed May 11 10:07:28 2005
-// $Revision: 1.1 $
-// $Id: Version.h,v 1.1 2005-05-20 15:28:46 thibaup Exp $
+// $Revision: 1.2 $
+// $Id: Version.h,v 1.2 2005-07-18 20:12:30 thibaup Exp $
 // 
 
 
@@ -22,42 +22,74 @@ using namespace std;
 
 namespace mccore
 {
+  class iBinstream;
+  class oBinstream;
+
   /**
-   * @short Library version
+   * @short Library version manipulation
    * 
    * @author Philippe Thibault <philippe.thibault@umontreal.ca>
    */
   class Version 
   {
+    /**
+     * Current version string "<major>.<minor>"
+     */
+    string current_version;
 
+    /**
+     * Current CPU architecture string.
+     */
+    string current_cpu;
+
+    /**
+     * Current vendor string.
+     */
+    string current_vendor;
+
+    /**
+     * Current operating system name string.
+     */
+    string current_os;
+
+    /**
+     * Current building date.
+     */
+    string current_date;
+    
   public:
 
     /**
-     * Version string "<major>.<minor>"
+     * Library version string "<major>.<minor>"
      */
     static const string version;
 
     /**
-     * Machine architecture string.
+     * Library CPU architecture string.
      */
-    static const string machine;
+    static const string cpu;
 
     /**
-     * Kernel version string.
+     * Library vendor string.
      */
-    static const string kernel;
+    static const string vendor;
 
     /**
-     * Operating system name string.
+     * Library operating system name string.
      */
-    static const string osname;
+    static const string os;
+
+    /**
+     * Library building date.
+     */
+    static const string date;
 
     // LIFECYCLE ------------------------------------------------------------
 
     /**
      * Initializes the object.
      */
-    Version () { }
+    Version ();
 
     /**
      * Destructs the object.
@@ -79,12 +111,47 @@ namespace mccore
      */
     ostream& write (ostream& os) const;
 
+    /**
+     * Writes the object to a binary stream.
+     * @param obs The stream.
+     * @return The written stream.
+     */
+    oBinstream& write (oBinstream& obs) const;
+
+    /**
+     * Reads the object from a binary stream.
+     * @param ibs The stream.
+     * @return The read stream.
+     */
+    iBinstream& read (iBinstream& ibs);
+
   };
 
+  /**
+   * Writes the object to a binary stream.
+   * @param obs The stream.
+   * @param obj The object to write.
+   * @return The written stream.
+   */
+  oBinstream& operator<< (oBinstream &obs, const Version& obj);
+
+  /**
+   * Reads the object from a binary stream.
+   * @param ibs The stream.
+   * @param obj The object to read.
+   * @return The read stream.
+   */
+  iBinstream& operator>> (iBinstream &ibs, Version& obj);
 }
 
 namespace std
 {
+  /**
+   * Writes the object to a stream.
+   * @param obs The stream.
+   * @param obj The object to write.
+   * @return The written stream.
+   */
   ostream& operator<< (ostream &os, const mccore::Version& obj);
 }
 
