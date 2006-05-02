c4ed46771236c0ef7945f83e5ac5a8ae6f77b94c
larosem <larosem>
2006-05-02 19:32:07 +0000

    Added LGPL license headers.

--- mccore/lib/Version.cc
+++ mccore/lib/Version.cc
@@ -4,20 +4,36 @@
 //                  Université de Montréal.
 // Author           : Philippe Thibault <philippe.thibault@umontreal.ca>
 // Created On       : Wed May 11 10:07:28 2005
-// $Revision: 1.6 $
-// $Id: Version.cc,v 1.6 2006-01-26 21:19:55 thibaup Exp $
+// $Revision: 1.7 $
+// $Id: Version.cc,v 1.7 2006-05-02 19:32:07 larosem Exp $
 // 
+// This file is part of mccore.
+// 
+// mccore is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// mccore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with mccore; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
-
 
 #include <sstream>
 
 #include "Binstream.h"
 #include "Messagestream.h"
 #include "Version.h"
+
 
 
 namespace mccore
--- mccore/lib/Version.h
+++ mccore/lib/Version.h
@@ -4,20 +4,34 @@
 //                  Université de Montréal.
 // Author           : Philippe Thibault <philippe.thibault@umontreal.ca>
 // Created On       : Wed May 11 10:07:28 2005
-// $Revision: 1.5 $
-// $Id: Version.h,v 1.5 2006-01-26 21:19:55 thibaup Exp $
+// $Revision: 1.6 $
+// $Id: Version.h,v 1.6 2006-05-02 19:32:07 larosem Exp $
 // 
+// This file is part of mccore.
+// 
+// mccore is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// mccore is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with mccore; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
 #ifndef _mccore_Version_h_
 #define _mccore_Version_h_
 
-
 #include <iostream>
 #include <string>
 
-
 using namespace std;
+
 
 
 namespace mccore
