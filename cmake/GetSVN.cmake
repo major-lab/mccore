88cb5a5e5678a526af40e7af16659a0a7dc427e5
Jonathan Roy <jonathan.roy.1@umontreal.ca>
2011-12-15 21:34:58 +0000

    Add dependency to the subversion revision number
    Note: The svnversion.h header file is created but not used by the library (the config.h file is enough)

--- cmake/GetSVN.cmake
+++ cmake/GetSVN.cmake
@@ -5,8 +5,9 @@
 ## Copyright (C) 2008,2009,2010,2011 Université de Montréal
 ##
 
-INCLUDE(FindSubversion)
+include(FindSubversion)
 Subversion_WC_INFO(${SOURCE_DIR} MCCORE)
-FILE(WRITE svnversion.h.cmake "#define SVNVERSION ${MCCORE_WC_REVISION}\n")
-EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+#message(STATUS ${MCCORE_WC_INFO})
+file(WRITE svnversion.h.cmake "#define SVNVERSION ${MCCORE_WC_REVISION}\n")
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
       svnversion.h.cmake svnversion.h)
